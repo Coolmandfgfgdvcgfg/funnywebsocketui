32fc83f45d4f941701b4ace91aa37eeb80cab2b6
Coolmandfgfgdvcgfg <108906803+Coolmandfgfgdvcgfg@users.noreply.github.com>
2024-07-10 10:59:05 -0400

    Update webd.lua

--- webd.lua
+++ webd.lua
@@ -1,11 +1,8 @@
 local Services = setmetatable({}, { __index = function(Self, Key) return game.GetService(game, Key) end })
 local Client = Services.Players.LocalPlayer
-local SMethod = (WebSocket and WebSocket.connect)
-
-if not SMethod then return Client:Kick("Executor is too shitty.") end
 
 local Main = function()
-	local Success, WebSocket = pcall(SMethod, "ws://localhost:9000/")
+	local Success, WebSocket = pcall(websocket.connect, "ws://localhost:9000/")
     	local Closed = false
 
 	if not Success then return end
@@ -14,15 +11,6 @@ local Main = function()
 		Method = "Authorization",
 		Name = Client.Name
 	}))
-	task.spawn(function()
-		while Closed == false do
-			WebSocket:Send(Services.HttpService:JSONEncode({
-            		Method = "Ping",
-            		Timestamp = tick()  -- Include a timestamp if needed
-        		}))
-			task.wait(2)
-		end
-	end)
 	WebSocket.OnMessage:Connect(function(Unparsed)
 		local Parsed = Services.HttpService:JSONDecode(Unparsed)
 		
@@ -43,7 +31,11 @@ local Main = function()
     	end)
 
     repeat
-        wait()  
+        WebSocket:Send(Services.HttpService:JSONEncode({
+            		Method = "Ping",
+            		Timestamp = tick()  -- Include a timestamp if needed
+        }))
+	task.wait(2)
     until Closed
 end
 
